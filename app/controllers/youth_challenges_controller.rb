a7fdb4ef67895d8bdaaf19bdabd6fc863fe74439
keoshayoung <keoshayoung@email.com>
2022-08-12 16:43:00 -0400

    added youth_challenge seed data and controller

---
+++ app/controllers/youth_challenges_controller.rb
@@ -0,0 +1,2 @@
+class YouthChallengesController < ApplicationController
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,108 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+youth_challenges = YouthChallenge.create!(name: "Hawaii NG Youth Challenge Academy", address: "1046 Leilani St, Bldg 621
+Hilo, HI 96720-8210", website: "http://dod.hawaii.gov/yca/")
+
+youth_challenges = YouthChallenge.create!(name: "Hawaii NG Youth Challenge Academy", address: "Building 1787 Shangrila Street
+Kapolei, HI 96707-0348", website: "http://dod.hawaii.gov/yca/")
+
+youth_challenges = YouthChallenge.create!(name: "Washington Youth Challenge Academy", address: "1207 Carver St NW
+Bremerton, WA 98312", website: "https://mil.wa.gov/youth-academy")
+
+youth_challenges = YouthChallenge.create!(name: "Oregon NG Youth Challenge Program", address: "23861 Dodds Road
+Bend, OR 97701", website: "https://www.oycp.com/")
+
+youth_challenges = YouthChallenge.create!(name: "Discovery Challenge Academy", address: "700 East Roth Rd
+Lathrop, CA 95330", website: "https://iamdiscovery.org/")
+
+youth_challenges = YouthChallenge.create!(name: "Grizzly Academy", address: "721 Mendocino Ave
+San Luis Obispo, CA 93405", website: "https://www.grizzlyyouthacademy.org/")
+
+youth_challenges = YouthChallenge.create!(name: "Sunburst Youth Challenge Academy", address: "4022 Saratoga Ave, Bldg 21
+Los Alamitos, CA 90720", website: "https://sunburstyouthacademy.com/")
+
+youth_challenges = YouthChallenge.create!(name: "Idaho Youth Challenge Academy", address: "117 Timberline Drive
+Pierce, ID 83546", website: "http://www.idyouthchallenge.com/")
+
+youth_challenges = YouthChallenge.create!(name: "Battle Born Youth Challenge Academy", address: "100 University Ave.
+Carlin, NV. 89822", website: "https://nvng.nv.gov/BBYCA")
+
+youth_challenges = YouthChallenge.create!(name: "Wyoming Youth Challenge Academy", address: "1502 Hwy 26 Building 505 
+Camp Guernsey, WY 82214", website: "https://wycowboychallenge.org/")
+
+youth_challenges = YouthChallenge.create!(name: "New Mexico Youth Challenge Academy", address: "131 Earl Cummings Loop
+Roswell, NM 88203", website: "http://www.nmyca.net/")
+
+youth_challenges = YouthChallenge.create!(name: "Thunderbird Challenge Program", address: "417 S. Elliot, Bldg 304
+Pryor, OK 74361", website: "http://thunderbird.org/")
+
+youth_challenges = YouthChallenge.create!(name: "Texas Challenge Academy", address: "600 HWY 3013 West
+Eagle Lake, TX 77434", website: "https://tmd.texas.gov/texas-challenge-academy")
+
+youth_challenges = YouthChallenge.create!(name: "Louisiana National Guard Youth Challenge Program", address: "5445 Point Clair, Bldg 20
+Carville, LA 70721", website: "https://langycp.com/")
+
+youth_challenges = YouthChallenge.create!(name: "Louisiana National Guard Youth Challenge Program", address: "505 F Street, Camp Beauregard
+Pineville, LA 71360", website: "https://langycp.com/")
+
+youth_challenges = YouthChallenge.create!(name: "Louisiana National Guard Youth Challenge Program", address: "200 Louisiana Blvd
+Camp Minden, LA 71055", website: "https://langycp.com/")
+
+youth_challenges = YouthChallenge.create!(name: "Arkansas Youth Challenge", address: "Camp J.T. Robinson, Bldg 16414
+North Little Rock, AR 72199-9600", website: "https://www.aryouthchallenge.com/")
+
+youth_challenges = YouthChallenge.create!(name: "Mississippi Youth Challenge", address: "Building 80 Halloran Ave
+Camp Shelby, MS 39407-5500", website: "http://msyouthchallenge.org/")
+
+youth_challenges = YouthChallenge.create!(name: "Lincoln's Challenge Academy", address: "212 W. Borman
+Rantoul, IL 61866", website: "http://www.lincolnschallenge.org/")
+
+youth_challenges = YouthChallenge.create!(name: "Wisconsin Challenge Academy", address: "749 East 12th Avenue
+Fort McCoy, WI 54656", website: "https://www.challengeacademy.org/")
+
+youth_challenges = YouthChallenge.create!(name: "Michigan Youth Challenge", address: "5500 Armstrong Road, Bldg #13
+Battle Creek, MI 49037", website: "https://www.michigan.gov/myca")
+
+youth_challenges = YouthChallenge.create!(name: "Hoosier Youth Challenge Academy", address: "10892 North State Road 140
+Knightstown, IN 46148-9718", website: "https://www.in.gov/ago/hyca/")
+
+youth_challenges = YouthChallenge.create!(name: "Bluegrass Challenge Academy", address: "114 Conroy Ave, bldg # 5549
+Fort Knox, KY 40121", website: "https://www.bcachallenge.com/")
+
+youth_challenges = YouthChallenge.create!(name: "Appalachian Challenge Academy", address: "465 Grays Drive
+Gray's Knob, KY 40829", website: "https://www.challengeaca.com/")
+
+youth_challenges = YouthChallenge.create!(name: "Georgia Youth Challenge Academy", address: "574 40th Street
+Fort Gordon, GA 30905", website: "https://www.georgiayouthchallenge.org")
+
+youth_challenges = YouthChallenge.create!(name: "Florida Youth Challenge Academy", address: "5629 State Road 16 West,
+Bldg 3800
+Starke, FL 32091", website: "https://www.floridayouthchallengeacademy.org/")
+
+youth_challenges = YouthChallenge.create!(name: "South Carolina Youth Challenge Academy", address: "5471 Leesburg Road Bldg 3892
+Eastover, SC 29044", website: "https://scyouthchallenge.com/")
+
+youth_challenges = YouthChallenge.create!(name: "Tar Heel Challenge Academy", address: "215 N. Main St
+New London, NC 28127", website: "https://nc-tcachallenge.org/")
+
+youth_challenges = YouthChallenge.create!(name: "Mountaineer Challenge Academy South", address: "305 Fayette Pike
+Montgomery, WV 25136", website: "http://wvchallenge.org/")
+
+youth_challenges = YouthChallenge.create!(name: "Mountaineer Challenge Academy North", address: "1001 Army Road, Building 443
+Kingwood, WI 26537-1077", website: "http://wvchallenge.org/")
+
+youth_challenges = YouthChallenge.create!(name: "Virginia Commonwealth Challenge Youth Academy", address: "253C Street Camp Pendleton
+Virginia Beach, VA 23451", website: "http://www.vachallenge.org/")
+
+youth_challenges = YouthChallenge.create!(name: "Capital Guardian Youth Challenge", address: "3201 Oak Hill Drive
+Laurel, DC 20724", website: "http://www.cgyca.org/")
+
+youth_challenges = YouthChallenge.create!(name: "Freestate Challenge Program", address: "Bldg #E4230, Beal Rd, APG Edgewood Area
+Gunpowder, MD 21210", website: "https://freestatemil.maryland.gov/")
+
+youth_challenges = YouthChallenge.create!(name: "Keystone State Challenge Academy", address: "Bldg. 0-13, Fort Indiantown Gap
+Annville, PA 17003", website: "http://www.dmva.pa.gov/KeystoneStateChallengeAcademy")
+
+youth_challenges = YouthChallenge.create!(name: "New Jersey Challenge Youth Program", address: "100 Camp Drive
+Sea Girt, NJ 08750", website: "https://njyca.org/")
+
+youth_challenges = YouthChallenge.create!(name: "Alaska Military Youth Academy", address: "Bldg. 60702 COL Wilfred Wood Dr., Camp Carroll
+  JBER, AK 99505-5727", website: "https://dmva.alaska.gov/AMYA/")
