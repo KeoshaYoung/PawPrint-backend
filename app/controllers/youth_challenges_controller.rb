333172f4bf63dbe578d126078fed1917ef09b44f
keoshayoung <keoshayoung@email.com>
2022-08-12 17:38:30 -0400

    added index action to youth challenge controller and added youth challenge views

--- app/controllers/youth_challenges_controller.rb
+++ app/controllers/youth_challenges_controller.rb
@@ -1,2 +1,6 @@
 class YouthChallengesController < ApplicationController
+  def index
+    youth_challenges = YouthChallenge.all
+    render json: youth_challenges.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
+  get "/youth_challenges" => "youth_challenges#index"
   post "/users" => "users#create"
-
   post "/sessions" => "sessions#create"
 end
