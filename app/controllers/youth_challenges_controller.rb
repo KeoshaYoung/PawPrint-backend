05492cf12999f857aa3809e0c23cc10ffb3926b2
keoshayoung <keoshayoung@email.com>
2022-08-19 14:13:27 -0400

    added youth challenges index, show, update,and destroy actions routes and requests

--- app/controllers/youth_challenges_controller.rb
+++ app/controllers/youth_challenges_controller.rb
@@ -1,6 +1,29 @@
 class YouthChallengesController < ApplicationController
   def index
-    youth_challenges = YouthChallenge.all
-    render json: youth_challenges.as_json
+    @youth_challenges = YouthChallenge.all.order(:id)
+    render template: "youth_challenges/index"
+  end
+
+  def show
+    @youth_challenge = YouthChallenge.find_by(id: params[:id])
+    render :show
+  end
+
+  def update
+    @youth_challenge = YouthChallenge.find_by(id: params[:id])
+    @youth_challenge.name = params[:name] || @youth_challenge.name
+    @youth_challenge.address = params[:address] || @youth_challenge.address
+    @youth_challenge.website = params[:website] || @youth_challenge.website
+    if @youth_challenge.save
+      render :show
+    else
+      render json: { errors: youth_challenge.errors.full_messages }, status: 418
+    end
+  end
+
+  def destroy
+    youth_challenge = YouthChallenge.find_by(id: params[:id])
+    youth_challenge.destroy
+    render json: { message: "Youth Challenge successfully destroyed!" }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get "/youth_challenges" => "youth_challenges#index"
   post "/users" => "users#create"
   post "/sessions" => "sessions#create"
+
+  get "/youth_challenges" => "youth_challenges#index"
+  get "/youth_challenges/:id" => "youth_challenges#show"
+  patch "/youth_challenges/:id" => "youth_challenges#update"
+  delete "/youth_challenges/:id" => "youth_challenges#destroy"
 end
